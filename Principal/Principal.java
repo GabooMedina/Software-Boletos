d30476fabca07569742ccacad61f9f3d5a9da6f8
Gabriel Medina <gmedina3108@uta.edu.ec>
2023-01-15 17:02:55 -0500

    Ferxxo

--- Principal/Principal.java
+++ Principal/Principal.java
@@ -2,5 +2,6 @@ public class Principal{
 public static void main(String[] args) {
     System.out.println("EL Main del Proyecto");
     System.out.println("Cosillo");
+    System.out.println(" No se demoren XD ");
 }
 }
