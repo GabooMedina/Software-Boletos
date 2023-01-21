8a5771cbf8545ad996cbf14ac30ea7bb13892fc2
Gabriel Medina <gmedina3108@uta.edu.ec>
2023-01-21 17:00:27 -0500

    Gestion Boleto

---
+++ Gestion_Boletos/Boletos.java
@@ -0,0 +1,60 @@
+public class Boletos {
+    
+    private String nombre;
+    private String apellido;
+    private String direccion;
+    private String cooperativa;
+    private String destino;
+    private String telefono;
+    private int edad;
+
+    public Boletos(){
+        
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getApellido() {
+        return this.apellido;
+    }
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    public String getDireccion() {
+        return this.direccion;
+    }
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    public String getCooperativa() {
+        return this.cooperativa;
+    }
+    public void setCooperativa(String cooperativa) {
+        this.cooperativa = cooperativa;
+    }
+    public String getDestino() {
+        return this.destino;
+    }
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+    public String getTelefono() {
+        return this.telefono;
+    }
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    public int getEdad() {
+        return this.edad;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+
+    
+}
