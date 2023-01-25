2d00596d273494c549ac1fe2aeb1892869b2c648
Gabriel Medina <gmedina3108@uta.edu.ec>
2023-01-25 12:17:24 -0500

    Clase Ruta

---
+++ GestionRutas/IngresoRutas.java
@@ -0,0 +1,5 @@
+package GestionRutas;
+
+public class IngresoRutas {
+    
+}
---
+++ GestionRutas/Ruta.java
@@ -0,0 +1,22 @@
+package GestionRutas;
+
+public class Ruta {
+    private String origen;
+    private String destino;
+
+    public String getOrigen() {
+        return this.origen;
+    }
+    public void setOrigen(String origen) {
+        this.origen = origen;
+    }
+    public String getDestino() {
+        return this.destino;
+    }
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+    
+    
+
+}
