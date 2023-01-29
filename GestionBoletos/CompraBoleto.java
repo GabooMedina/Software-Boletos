4576e3809915020e4c9de1778ceb9ebfaf77e98c
CarlosAl21 <calvarado2731@uta.edu.ec>
2023-01-29 11:18:09 -0500

    Boletos

--- GestionBoletos/CompraBoleto.java
+++ GestionBoletos/CompraBoleto.java
@@ -23,9 +23,9 @@ public class CompraBoleto {
             while (g.getRs().next()) {
 
                 System.out.println("_________________________________________________________________________________________");
-                System.out.println("Cooperativa"+"\tOrigen"+"\tDestino"+"\tHorario\n"+
-                "_________________________________________________________________________________________\n"+ 
-                g.getRs().getString("Cooperativa")+"\t\t"+g.getRs().getString("Origen")+"\t\t"+g.getRs().getString("Destino")+"\t\t"+g.getRs().getString("Horario")+"\n");
+                System.out.printf("Id\t\tRutaCooperativa\tOrigen\t\tDestino\t\tHorario\n"+
+                "_________________________________________________________________________________________\n"+
+                g.getRs().getString("Id_Rutas")+"\t\t"+g.getRs().getString("Cooperativa")+"\t\t"+g.getRs().getString("Origen")+"\t\t"+g.getRs().getString("Destino")+"\t\t"+g.getRs().getString("Horario")+"\n"); 
                 /*System.out.println("Cooperativa: " + g.getRs().getNString("Cooperativa"));
                 System.out.println("Origen: " + g.getRs().getNString("Origen"));
                 System.out.println("Destino: " + g.getRs().getNString("Destino"));
