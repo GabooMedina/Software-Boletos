d9d4f5ad2e8dce2ab6d830a089f11267d619b5b6
CarlosAl21 <calvarado2731@uta.edu.ec>
2023-01-30 11:28:07 -0500

    Objeto Factura

--- Factura/Factura.java
+++ Factura/Factura.java
@@ -1,7 +1,19 @@
 package Factura;
 
-public class Factura {
+import java.sql.Date;
 
+public class Factura {
+    int idUsuario;
+    int id;
+    String coopertiva;
+    String origen;
+    String destino;
+    String horario;
+    Double precio;
+    int asiento;
+    Date fecha;
+
+    
     public boolean ImpresionFactura(String cooperativa, String origen, String destino, String horario, Double precio){
         System.out.println(cooperativa+origen+destino+horario);
         return true;
