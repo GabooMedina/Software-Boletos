55ff54ab72a6ca4f87756778586dc3fbb1bb15c6
CarlosAl21 <calvarado2731@uta.edu.ec>
2023-01-30 16:59:06 -0500

    aaaaaa

---
+++ Menus/Menus.java
@@ -0,0 +1,6 @@
+package Menus;
+
+public class Menus {
+    
+    public static MenuUsuario(Usuarios u, Asiento,)
+}
--- Menus/main.java
+++ Menus/main.java
@@ -1,5 +1,10 @@
 package Menus;
 
+import Login.Login;
+
 public class main {
-    
+    public static void main(String[] args) {
+        Login inicio=new Login();
+        inicio.menuLogin();
+    }
 }
